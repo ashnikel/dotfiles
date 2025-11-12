8cd76570d2ec5e99146b49540d237a194e124d67
Viktor Medvedik <ashnikel@gmail.com>
2025-11-12 12:42:29 +0300

    wip

--- home.nix
+++ home.nix
@@ -11,18 +11,21 @@
      dysk
      eza
      fastfetch
+     file
      fd
      hyprpaper
      moor
      obsidian
      pcmanfm
+     qview
      ripgrep
      starship
      telegram-desktop
      tealdeer
      walker
      waybar
-     xclip
+     wl-clipboard
+     zathura
      zed-editor
     ];
 
@@ -47,17 +50,57 @@
 	gs = "git status";
 	tree = "exa -lT --icons";
 	v = "nvim";
+        p = "wl-paste";
+        pb = "wl-paste | head";
       };
       shellInit = ''
         set -gx EDITOR nvim
       '';
+
+    # Пользовательские функции
+
+  # Функция c — копирует файл, если указан
+  functions.c = ''
+    if test (count $argv) -eq 0
+        # Нет аргументов — читаем из stdin
+        wl-copy
+    else
+        set file $argv[1]
+        if test -f "$file"
+            echo "📋 Копирую файл: $file"
+            cat "$file" | wl-copy
+        else
+            echo "❌ Файл не найден: $file"
+            return 1
+        end
+    end
+  '';
+
+ # Функция clast — копирует последний изменённый текстовый файл
+    functions.clast = ''
+      set last (eza --sort=modified --reverse | head -n 1)
+
+      if test -z "$last"
+          echo "❌ Нет файлов в текущей директории"
+          return 1
+      end
+
+      set mimetype (file --mime-type -b "$last")
+
+      if string match -q "text/*" $mimetype
+          echo "📋 Копирую текстовый файл: $last ($mimetype)"
+          cat "$last" | wl-copy
+      else
+          echo "⚠️ Файл $last не текстовый ($mimetype) — пропускаю"
+      end
+    '';
     };
 
     programs.git = {
       enable = true;
       settings.user.name = "Viktor Medvedik";
       settings.user.email = "ashnikel@gmail.com";
-      extraConfig.init.defaultBranch = "main";
+      settings.init.defaultBranch = "main";
     };
 
     programs.kitty = {
@@ -77,6 +120,21 @@
 
     programs.starship.enable = true;
     programs.zoxide.enable = true;
+
+
+    services.hyprpaper = {
+      enable = true;
+      settings = {
+        preload = [
+          "~/dotfiles/wallpapers/01.png"
+        ];
+        wallpaper = [
+          "eDP-1,~/dotfiles/wallpapers/01.png"
+        ];
+        splash = false;
+        ipc = "off";
+      };
+    };
 
 #    programs.home-manager.enable = true;
 }
